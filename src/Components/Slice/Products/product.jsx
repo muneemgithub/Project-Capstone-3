9d157e32756d26ab17e3357736c072b54c81b6e7
muneemgithub <muneemahmed456@gmail.com>
2024-12-17 18:04:40 +0500

    dummy data

--- src/Components/Slice/Products/product.jsx
+++ src/Components/Slice/Products/product.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppleImg from "../../Assests/ApplesImage.webp"
 import BlueberriesImg from "../../Assests/blueberriesImage.webp"
 import ClementinesImg from "../../Assests/clementinesImage.webp"
@@ -28,6 +28,38 @@ import SlicedturkeybreastImg from "../../Assests/sliced_turkey_breastImage.webp"
 import ChickenthighImg from "../../Assests/chicken_thighimage.webp"
 import ChickenbrestImg from "../../Assests/chicken_brestImage.webp"
 import StickImg from "../../Assests/steakImage.webp"
+import BelmontcustardcreamImg from "../../Assests/belmont_custard_creamImage.webp"
+import CrawforddigestivesImg from "../../Assests/crawford_digestivesImage.webp"
+import CrawfordshortieImg from "../../Assests/crawford_shortieImage.webp"
+import HillBiscuitsImg from "../../Assests/Hill_Biscuits_Mini_Pack_MixImage.webp"
+import JammieDodgersImg from "../../Assests/Jammie_DodgersImage.webp"
+import JulischeeseImg from "../../Assests/julis_cheese_crackersImage.webp"
+import KhongguanImg from "../../Assests/khong_guan_orange_creamImage.webp"
+import KhongguanpremiumImg from "../../Assests/khong_guan_premium_marieImage.webp"
+import LotusbiscoffImg from "../../Assests/lotus_biscoffImage.webp"
+import ArtiachfiliImg from "../../Assests/artiach_filipinoImage.webp"
+import Grain_freeImg from "../../Assests/grain_freeImage.webp"
+import HillsscienceImg from "../../Assests/hills_science_dietImage.webp"
+import InstinctImg from "../../Assests/instinct_the_rawImage.webp"
+import NaturalImg from "../../Assests/natural_balanceImage.webp"
+import PrescriptionImg from "../../Assests/prescription_dietImage.webp"
+import Pro_dietImg from "../../Assests/pro_dietImage.webp"
+import Purina_proImg from "../../Assests/purina_pro_planImage.webp"
+import Dog_food_blackImg from "../../Assests/dog_food_black_hawk_adult_all_breeds_lamb&riceImage.webp"
+import Dog_food_black_hawkImg from "../../Assests/dog_food_black_hawk_puppy_all_breeds_lamb&riceImage.webp"
+import DogfoodblackhawkImg from "../../Assests/dog_food_black_hawk_puppy_large_breedchicken&riceImage.webp"
+import Air_freshner_acana_ozmoImg from "../../Assests/air_freshner_acana_ozmoImage.webp"
+import Air_freshner_air_wick_essentialImg from "../../Assests/air_freshner_air_wick_essential_oilsImage.webp"
+import Air_freshner_ambipurImg from "../../Assests/air_freshner_ambipur_air_sweet_citrus&zestImage.webp"
+import Air_freshner_febreze_air_boraImg from "../../Assests/air_freshner_febreze_air_bora_boraImage.webp"
+import Air_freshner_febreze_air_effectsImg from "../../Assests/air_freshner_febreze_air_effectsImage.webp"
+import Air_freshner_febreze_air_hawaiianImg from "../../Assests/air_freshner_febreze_air_hawaiianImage.webp"
+import Air_freshner_sc_johnson_gladeImg from "../../Assests/air_freshner_sc_johnson_glade_blue_odysseyImage.webp"
+import Air_freshner_sc_johnson_glade_cleanImg from "../../Assests/air_freshner_sc_johnson_glade_clean_linenImage.webp"
+import Cleaning_supply_astonishImg from "../../Assests/cleaning_supply_astonishImage.webp"
+import Cleaning_supply_bonaImg from "../../Assests/cleaning_supply_bonaImage.webp"
+import Cleaning_supply_everImg from "../../Assests/cleaning_supply_ever_springImage.webp"
+import Cleaning_supply_glitz_steelImg from "../../Assests/cleaning_supply_glitz_steel_cleanerImage.webp"
 import { Box,  Button,  Typography } from '@mui/material'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
@@ -37,78 +69,91 @@ const dummydata = [
         name: "Apples",
         Image: AppleImg,
         Price: 1.60,
+        category: "Fruit",
     },
     {
         id:  2,
         name: "Blueberries",
         Image: BlueberriesImg,
         Price: 3.00,
+        category: "Fruit",
     },
     {
         id:  3,
         name: "Clementines",
         Image: ClementinesImg,
         Price: 2.50,
+        category: "Fruit",
     },
     {
         id:  4,
         name: "Dates",
         Image: DatesImg,
         Price: 8.00,
+        category: "Fruit",
     },
     {
         id:  5,
         name: "GreenBeans",
         Image: GreenBeansImg,
         Price: 1.20,
+        category: "Fruit",
     },
     {
         id:  6,
         name: "Mangoes",
         Image: MangoesImg,
         Price: 2.50,
+        category: "Fruit",
     },
     {
         id:  7,
         name: "Pears",
         Image: PearsImg,
         Price: 3.50,
+        category: "Fruit",
     },
     {
         id:  8,
         name: "RedCherries",
         Image: RedCherriesImg,
         Price: 1.80,
+        category: "Fruit",
     },
     {
         id:  9,
         name: "Strawberry",
         Image: StrawberryImg,
         Price: 8.00,
+        category: "Fruit",
     },
     {
         id:  10,
         name: "BabySpinach",
         Image: BabySpinachImg,
         Price: 0.60,
+        category: "Fruit",
     },
     {
         id:  11,
         name: "BrusselsSprouts",
         Image: BrusselsSproutsImg,
         Price: 0.60,
+        category: "Fruit",
     },
     {
         id:  12,
         name: "Corn",
         Image: CornImg,
         Price: 4.00,
+        category: "Fruit",
     },
     {
         id:  13,
         name: "Cucumber",
         Image: CucumberImg,
         Price: 4.00,
+        category: "Fruit",
     },
     {
         id:  14,
@@ -121,153 +166,417 @@ const dummydata = [
         name: "GreenLimes",
         Image: GreenLimesImg,
         Price: 1.50,
+        category: "Fruit",
     },
     {
         id:  16,
         name: "MiniPeppers",
         Image: MiniPeppersImg,
         Price: 5.00,
+        category: "Fruit",
     },
     {
         id:  17,
         name: "PeeledCarrots",
         Image: PeeledCarrotsImg,
         Price: 2.20,
+        category: "Fruit",
     },
     {
         id:  18,
         name: "VeggiePlatter",
         Image: VeggiePlatterImg,
         Price: 3.20,
+        category: "Fruit",
     },
     {
         id:  19,
         name: "YellowLimes",
         Image: YellowLimesImg,
         Price: 1.20,
+        category: "Fruit",
     },
     {
         id:  20,
         name: "SignatureSalmon",
         Image: SignatureSalmonImg,
         Price: 4.95,
+        category: "Meat",
     },
     {
         id:  21,
         name: "Codfillet",
         Image: CodfilletImg,
         Price: 7.50,
+        category: "Meat",
     },
     {
         id:  22,
         name: "Swordfish",
         Image: SwordfishImg,
         Price: 7.50,
+        category: "Meat",
     },
     {
         id:  23,
         name: "Halibut",
         Image: HalibutImg,
         Price: 12.00,
+        category: "Meat",
     },
     {
         id:  24,
         name: "TilapiaFillet",
         Image: TilapiaFilletImg,
         Price: 7.89,
+        category: "Meat",
     },
     {
         id:  25,
         name: "Beef",
         Image: BeefImg,
         Price: 6.00,
+        category: "Meat",
     },
     {
         id:  26,
         name: "Slicedturkeybreast",
         Image: SlicedturkeybreastImg,
         Price: 7.50,
+        category: "Meat",
     },
     {
         id:  27,
         name: "Chickenthigh",
         Image: ChickenthighImg,
         Price: 7.89,
+        category: "Meat",
     },
     {
         id:  28,
         name: "Chickenbrest",
         Image: ChickenbrestImg,
         Price: 9.00,
+        category: "Meat",
     },
     {
         id:  29,
         name: "StickImg",
         Image: StickImg,
         Price: 12.00,
+        category: "Meat", 
+    },
+    {
+        id:  30,
+        name: "Belmontcustardcream",
+        Image: BelmontcustardcreamImg,
+        Price: 5.00,
+        category: "Snacks", 
+    },
+    {
+        id:  31,
+        name: "Crawforddigestives",
+        Image: CrawforddigestivesImg,
+        Price: 6.50,
+        category: "Snacks", 
+    },
+    {
+        id:  32,
+        name: "Crawfordshortie",
+        Image: CrawfordshortieImg,
+        Price: 5.00,
+        category: "Snacks", 
+    },
+    {
+        id:  33,
+        name: "HillBiscuits",
+        Image: HillBiscuitsImg,
+        Price: 7.50,
+        category: "Snacks", 
+    },
+    {
+        id:  34,
+        name: "JammieDodgers",
+        Image: JammieDodgersImg,
+        Price: 7.00,
+        category: "Snacks", 
+    },
+    {
+        id:  35,
+        name: "Julischeese",
+        Image: JulischeeseImg,
+        Price: 5.00,
+        category: "Snacks", 
+    },
+    {
+        id:  36,
+        name: "Khongguan",
+        Image: KhongguanImg,
+        Price: 5.00,
+        category: "Snacks", 
+    },
+    {
+        id:  37,
+        name: "Khongguanpremium",
+        Image: KhongguanpremiumImg,
+        Price: 6.00,
+        category: "Snacks", 
+    },
+    {
+        id:  38,
+        name: "Lotusbiscoff",
+        Image: LotusbiscoffImg,
+        Price: 7.50,
+        category: "Snacks", 
+    },
+    {
+        id:  39,
+        name: "Artiachfili",
+        Image: ArtiachfiliImg,
+        Price: 4.00,
+        category: "Snacks", 
+    },
+    {
+        id:  40,
+        name: "Grain_free",
+        Image: Grain_freeImg,
+        Price: 25.59,
+        category: "Pet Care", 
+    },
+    {
+        id:  41,
+        name: "HillsscienceImg",
+        Image: HillsscienceImg,
+        Price: 25.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  42,
+        name: "Instinct",
+        Image: InstinctImg,
+        Price: 18.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  43,
+        name: "Natural",
+        Image: NaturalImg,
+        Price: 14.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  44,
+        name: "Prescription",
+        Image: PrescriptionImg,
+        Price: 16.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  45,
+        name: "Pro_diet",
+        Image: Pro_dietImg,
+        Price: 18.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  46,
+        name: "Purina_pro",
+        Image: Purina_proImg,
+        Price: 25.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  47,
+        name: "Dog_food_black",
+        Image: Dog_food_blackImg,
+        Price: 24.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  48,
+        name: "Dog_food_black_hawk",
+        Image: Dog_food_black_hawkImg,
+        Price: 25.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  49,
+        name: "Dogfoodblackhawk",
+        Image: DogfoodblackhawkImg,
+        Price: 25.00,
+        category: "Pet Care", 
+    },
+    {
+        id:  50,
+        name: "Air_freshner_acana_ozmo",
+        Image: Air_freshner_acana_ozmoImg,
+        Price: 15.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  51,
+        name: "DogfoodblackAir_freshner_air_wick_essential",
+        Image: Air_freshner_air_wick_essentialImg,
+        Price: 20.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  52,
+        name: "Air_freshner_ambipur",
+        Image: Air_freshner_ambipurImg,
+        Price: 15.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  53,
+        name: "Air_freshner_febreze_air_bora",
+        Image: Air_freshner_febreze_air_boraImg,
+        Price: 15.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  54,
+        name: "Air_freshner_febreze_air_effects",
+        Image: Air_freshner_febreze_air_effectsImg,
+        Price: 15.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  55,
+        name: "Air_freshner_febreze_air_hawaiian",
+        Image: Air_freshner_febreze_air_hawaiianImg,
+        Price: 18.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  56,
+        name: "Air_freshner_sc_johnson_glade",
+        Image: Air_freshner_sc_johnson_gladeImg,
+        Price: 18.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  57,
+        name: "Air_freshner_sc_johnson_glade_clean",
+        Image: Air_freshner_sc_johnson_glade_cleanImg,
+        Price: 18.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  58,
+        name: "Cleaning_supply_astonish",
+        Image: Cleaning_supply_astonishImg,
+        Price: 15.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  59,
+        name: "Cleaning_supply_bonaImg",
+        Image: Cleaning_supply_bonaImg,
+        Price: 15.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  60,
+        name: "Cleaning_supply_ever",
+        Image: Cleaning_supply_everImg,
+        Price: 10.00,
+        category: "Home & Cleaning", 
+    },
+    {
+        id:  61,
+        name: "Cleaning_supply_glitz_steel",
+        Image: Cleaning_supply_glitz_steelImg,
+        Price: 10.00,
+        category: "Home & Cleaning", 
     }
 ]
 const Products = () => {
-  return (
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
-    <>
-      {
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "16px", justifyContent: "space-between", marginTop: 20 }}>
-      {dummydata.map((item) => (
-        <Box
-          key={item.id}
-          style={{
-            width: "270px",
-            height: "400px",
-            textAlign: "center",
-            background: "white",
-            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-            borderRadius: "5px",
-            padding: "16px",
-            border: "1px solid #ddd",
-          }}
-        >
-          <img
-            src={item.Image}
-            alt={item.name}
-            style={{ width: "100%", height: "auto", borderRadius: "5px" }}
-          />
-          <Typography style={{ fontWeight: "bold", marginTop: "8px" }}>
-            {item.name}
-          </Typography>
-          <Typography
-            style={{
-              color: "green",
-              fontWeight: "bold",
-              margin: "8px 0",
-            }}
-          >
-            ${item.Price}
-          </Typography>
+    // Filter products based on selected category
+    const filteredProducts = dummydata.filter((item) =>
+      selectedCategory === 'All' || item.category === selectedCategory
+    );
+    return (
+      <>
+        {/* Category Selection Buttons */}
+        <div style={{ marginBottom: '20px' }}>
           <Button
-            variant="contained"
-            style={{
-              backgroundColor: "green",
-              color: "white",
-              borderRadius: "5px",
-              display: "flex",
-              alignItems: "center",
-              gap: "8px",
-            }}
-            startIcon={<ShoppingCartIcon />}
+            onClick={() => setSelectedCategory('All')}
+            variant={selectedCategory === 'All' ? 'contained' : 'outlined'}
+            style={{ marginRight: '10px' }}
           >
-            Add to Cart
+            All
           </Button>
-        </Box>
-      ))}
-    </div>
-     
-      
-
-        
-      }
-    </>
-  )
+          <Button
+            onClick={() => setSelectedCategory('Fruit')}
+            variant={selectedCategory === 'Fruit' ? 'contained' : 'outlined'}
+            style={{ marginRight: '10px' }}
+          >
+            Fruits
+          </Button>
+          <Button
+            onClick={() => setSelectedCategory('Meat')}
+            variant={selectedCategory === 'Meat' ? 'contained' : 'outlined'}
+          >
+            Meat
+          </Button>
+        </div>
+  
+        {/* Displaying filtered products */}
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px', justifyContent: 'space-between', marginTop: 20 }}>
+          {filteredProducts.map((item) => (
+            <Box
+              key={item.id}
+              style={{
+                width: '270px',
+                height: '400px',
+                textAlign: 'center',
+                background: 'white',
+                boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                borderRadius: '5px',
+                padding: '16px',
+                border: '1px solid #ddd',
+              }}
+            >
+              <img
+                src={item.Image}
+                alt={item.name}
+                style={{ width: '100%', height: 'auto', borderRadius: '5px' }}
+              />
+              <Typography style={{ fontWeight: 'bold', marginTop: '8px' }}>
+                {item.name}
+              </Typography>
+              <Typography
+                style={{
+                  color: 'green',
+                  fontWeight: 'bold',
+                  margin: '8px 0',
+                }}
+              >
+                ${item.Price}
+              </Typography>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: 'green',
+                  color: 'white',
+                  borderRadius: '5px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '8px',
+                }}
+                startIcon={<ShoppingCartIcon />}
+              >
+                Add to Cart
+              </Button>
+            </Box>
+          ))}
+        </div>
+      </>
+    );
+  
 }
 
 export default Products;
